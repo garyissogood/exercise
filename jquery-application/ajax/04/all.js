efbccc3080240648d51dffcbc0a65348c9b6c7fb
GaryKuo <garywork7172@gmail.com>
2020-03-12 16:15:03 +0800

    ajax篇ex.4

---
+++ jquery-application/ajax/04/all.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".menu>a").click(function (e) {
+    $(".menu>a.selected").removeClass();
+    $(".content").load($(this).addClass("selected").attr("href"));
+    e.preventDefault();
+  }).first().click();
+});
+
